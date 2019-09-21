02d7cf867fd7a5044688facf29bf497b82d5e9fe
xie0737 <2649593332@qq.com>
2019-09-21 15:32:29 +0800

    first commit

---
+++ src/main/java/com/demo/community0921/Community0921Application.java
@@ -0,0 +1,14 @@
+package com.demo.community0921;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class Community0921Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Community0921Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/community0921/Community0921ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.demo.community0921;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Community0921ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
