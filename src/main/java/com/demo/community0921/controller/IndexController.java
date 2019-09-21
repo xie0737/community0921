b0edc21437d467d90eb1a092d8fdf7373ebba08f
xie0737 <2649593332@qq.com>
2019-09-21 20:36:20 +0800

    extract github config from code to properties

--- src/main/java/com/demo/community0921/controller/IndexController.java
+++ src/main/java/com/demo/community0921/controller/IndexController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
-public class indexController {
+public class IndexController {
     @GetMapping("/")
     public String index(){
         return "index";
